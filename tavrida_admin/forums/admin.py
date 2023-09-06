8985977f46fbd9d79eaabb5965d3a0c84c494d39
pavivin <pavivin@yandex.ru>
2023-09-06 23:01:13 +0700

    fix image

--- tavrida_admin/forums/admin.py
+++ tavrida_admin/forums/admin.py
@@ -3,12 +3,19 @@ from django.utils.html import format_html
 
 from .models import Forum, Model
 
+UPLOAD_URL = 'http://185.233.187.109:8000/uploads'
+
 
 @admin.register(Forum)
 class ForumAdminView(admin.ModelAdmin):
     list_display = ("image_tag", "title", "started_at")
     readonly_fields = ("deleted_at",)
     fields = ("title", "description", "started_at", "ended_at", "image_urls", "map_urls")
+    
+    def save_model(self, request, obj: Model, form, change):
+        super().save_model(request, obj, form, change)
+        obj.logo_url = UPLOAD_URL + obj.logo_url.url
+        obj.save()
 
     def image_tag(self, obj: Forum):
         return format_html(f'<img src="{obj.logo_url}" style="max-width:200px; max-height:200px"/>')
@@ -29,6 +36,11 @@ class ModelAdminView(admin.ModelAdmin):
         "forum",
         "title",
     )
+
+    def save_model(self, request, obj: Model, form, change):
+        super().save_model(request, obj, form, change)
+        obj.logo_url = UPLOAD_URL + obj.logo_url.url
+        obj.save()
 
     def image_tag(self, obj: Model):
         return format_html(f'<img src="{obj.logo_url}" style="max-width:200px; max-height:200px"/>')
